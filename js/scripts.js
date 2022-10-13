9d9a2723d80c835bcfee3c4668fc49e8a3cf6a8c
Karolina Laskarzewska <kar.laskarzewska@gmail.com>
2022-10-13 18:15:30 +0200

    Add new files

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+'use script';
+var heightInput = document.querySelector(".height-input-field");
+var weightInput = document.querySelector(".weight-input-field");
+var calculateButton = document.querySelector(".calculate");
+var result = document.querySelector(".result");
+var statement = document.querySelector(".result-statement");
+var BMI, height, weight;
+
+calculateButton.addEventListener("click", ()=>{
+
+     height = heightInput.value;
+     weight = weightInput.value;
+     BMI = weight/(height/100)**2; 
+     result.innerText = BMI;
+ 
+     if(BMI < 18.5){
+         statement.innerText = "Underweight";    
+     }else if((BMI > 18.5) && (BMI < 24.9)){
+         statement.innerText = "Healthy";
+     }else if((BMI > 25) && (BMI < 29.9 )){
+         statement.innerText = "Overweight";
+     }else{
+         statement.innerText = "Obesity";
+     }
+ });
